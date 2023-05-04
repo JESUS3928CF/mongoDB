a46de96c316772f97dea4c3792b240279f10f08a
Jesús Cochero <jesus3928cf@gmail.com>
2023-05-03 21:31:43 -0500

    docs: :memo: avanse de las las consultas del taller 2

--- taller2.mongodb.js
+++ taller2.mongodb.js
@@ -1,7 +1,6 @@
-//! EJERCICIO 1
+//!  1) Usando la colección listingsAndReviews de sample_airbnb, encuentre mediante el uso de agregaciones, cuál es la propiedad con mayor número de servicios ("amenities") de la colección.
 
-//! 1.1 
-use("sample_airbnb");
+use('sample_airbnb');
 
 // db.listingsAndReviews.aggregate([
 //     { $project: {
@@ -20,7 +19,7 @@ use("sample_airbnb");
 //     }
 // ]);
 
-//! 1.2
+//! 2 Usando la colección listingsAndReviews de sample_airbnb, encuentre mediante el uso de agregaciones, el número de propiedades que tienen conexión a Internet, sea desde Wifi o desde cable (Ethernet). Nota. Revise el campo amenities (“servicios”)
 
 // db.listingsAndReviews.aggregate([
 //     {
@@ -41,32 +40,36 @@ use("sample_airbnb");
 //     }
 // ])
 
-//! 1.3
+//! 3) Usando la colección listingsAndReviews de sample_airbnb, encuentre mediante el uso de agregaciones, todas las propiedades que hayan recibido 50 o más comentarios, que la valoración ("review_score_rating") sea mayor o igual a 80, que cuenten con conexión a Internet vía cable y que estén ubicadas en Brazil.
 
 db.listingsAndReviews.aggregate([
-    
     {
         $project: {
-          "cantidadComenterios": {
-            $size: "$reviews"
-          },
-          // "reviews": true,
-          "review_scores": true,
-          review_scores_rating: "$review_scores.review_scores_rating"
-          
-        }
+            cantidadComenterios: {
+                $size: '$reviews',
+            },
+            // "reviews": true,
+            // "review_scores": true,
+            review_scores_rating: '$review_scores.review_scores_rating',
+        },
     },
     {
-      $match: {
-        $and: [
-          {"cantidadComenterios": {$gte:50}},
-          {review_scores_rating: { $gte: 80 }},
-      
-        ]
-      }
-  }
-])
+        $match: {
+            $and: [
+                { cantidadComenterios: { $gte: 50 } },
+                { review_scores_rating: { $gte: 80 } },
+            ],
+        },
+    },
+]);
 
 // db.listingsAndReviews.aggregate([])
 
+//* Utilizando la base datos de sample_restaurants, construir un tablero que nos permita mostrar:
+//! ¿Cuántos restaurantes hay en total?
 
+//! ¿Cuál es el distrito que cuenta con el mayor número de restaurantes?
+
+//! ¿Cuántos restaurantes hay por cada tipo de cocina?
+
+//! ¿Cuántos restaurantes hay por cada código postal?
